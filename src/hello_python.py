1e15f674268b0e4d5ff82bbdc70c98a1d6307c65
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-18 18:18:06 -0400

    completed

---
+++ src/hello_python.py
@@ -0,0 +1,48 @@
+
+def count_to_one_hundred():
+    for x in range(1, 101):
+        print(x)
+
+def stub_function():
+    pass
+
+def multiply_two_numbers(a, b):
+    return a * b
+
+def main():
+
+    # print('hello world!')
+
+    # a = 1
+    # b = 'Show me your Bankai'
+    # c = [1,2,3,4]
+    # d = (5,6,7,8)
+    # e= {'name': 'Brian', 'number': 1}
+
+    # print(a)
+    # print(b)
+    # print(c)
+    # print(d)
+
+    # print(len(c))
+    # c.append(42)
+    # print(c)
+    # c.append("will this work?")
+    # print(c)
+    # print(e)
+
+    # print(e['name'])
+    # e['number'] += 9000
+    # print(e['number'])
+
+    # for n in c:
+    #     for m in d:
+    #         print(n * m)
+
+    # count_to_one_hundred()
+
+    value = multiply_two_numbers(6,7)
+    print(value)
+
+if __name__ == "__main__":
+    main()
